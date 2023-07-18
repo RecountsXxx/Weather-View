5f02e52c9a610c94d04e001ea10ea5931c5c0b12
Bogdan <Bogdan@DESKTOP-PGENPK7>
2023-07-19 01:38:31 +0300

    added design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { NgxEchartsModule } from 'ngx-echarts';
+import { ChartComponent } from './chart/chart.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxEchartsModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
